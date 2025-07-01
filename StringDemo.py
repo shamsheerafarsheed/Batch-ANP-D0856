ef48c4831bc604611eb8902cb1ef3732a598f416
Shamsheera  A T <96409324+shamsheerafarsheed@users.noreply.github.com>
2025-07-01 20:24:49 +0530

    Add files via upload

---
+++ StringDemo.py
@@ -0,0 +1,19 @@
+text="Python"
+#accessing characters(Indexing)
+print(text[0])
+print(text[1])
+print(text[-1])
+print("-----------Traversing a string(Loop)--------")
+#Traversing a string(Loop)
+for ch in "Python":
+    print(ch)
+#String Slicing
+print("-----------Slicing--------")
+text1="Python is useful in Datascience"
+print(text1[10:17])
+print(text1[-12:-1])
+print(text1[:7])#will start from first
+print(text1[10:])#till end
+#to reverse String
+print(text1[::-1])
+
