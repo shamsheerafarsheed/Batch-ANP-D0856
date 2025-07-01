6c272330993a1a3374b5b50b6fab643d86592eaf
Shamsheera  A T <96409324+shamsheerafarsheed@users.noreply.github.com>
2025-07-01 19:39:22 +0530

    Add files via upload

--- FunctionDetail.py
+++ FunctionDetail.py
@@ -23,13 +23,31 @@ print("------Variable length Argument(* args,** args) -----------")
 #** args--->used to pass multiple key-value pair as dictionary
 def sum_all(*numbers):
     return sum(numbers)
-print(sum_all(1,2,3,4,5))#15
+print(sum_all(11,25,30,4,5))#15
 
 print("Keyword arguments")
 def person_info(**details):
     for key,value in details.items():
         print(f"{key}:{value}")
 person_info(name="Ananaya",age=21,city="Banglore")
+print("------Lambda Function-----------")
+#small one line function without name
+#Syntax:lambda argument:expression
+square=lambda s,t:s*t
+print(square(25,5))
+print("------Recursion Function-----------")
+#Recursion function call itself until base condition met
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(factorial(5))
+        
+
+
+
+
 
     
 
